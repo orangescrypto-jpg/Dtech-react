a1ece7a5c955d51063a5badf599065de943ff828
orangescrypto-jpg <orangescrypto@gmail.com>
2026-04-29 03:16:49 +0100

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,5 @@
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import { AuthProvider } from './context/AuthContext';
 import MainLayout from './layouts/MainLayout';
 import Home from './pages/Home';
 import About from './pages/About';
@@ -6,20 +7,33 @@ import BlogList from './pages/BlogList';
 import BlogDetail from './pages/BlogDetail';
 import Contact from './pages/Contact';
 import Privacy from './pages/Privacy';
+import AdminLogin from './pages/admin/AdminLogin';
+import AdminDashboard from './pages/admin/AdminDashboard';
+import PostEditor from './pages/admin/PostEditor';
+import ProtectedRoute from './components/ProtectedRoute';
 
 export default function App() {
   return (
-    <Router>
-      <Routes>
-        <Route path="/" element={<MainLayout />}>
-          <Route index element={<Home />} />
-          <Route path="about" element={<About />} />
-          <Route path="blog" element={<BlogList />} />
-          <Route path="blog/:id" element={<BlogDetail />} />
-          <Route path="contact" element={<Contact />} />
-          <Route path="privacy" element={<Privacy />} />
-        </Route>
-      </Routes>
-    </Router>
+    <AuthProvider>
+      <Router>
+        <Routes>
+          {/* Public Routes */}
+          <Route path="/" element={<MainLayout />}>
+            <Route index element={<Home />} />
+            <Route path="about" element={<About />} />
+            <Route path="blog" element={<BlogList />} />
+            <Route path="blog/:id" element={<BlogDetail />} />
+            <Route path="contact" element={<Contact />} />
+            <Route path="privacy" element={<Privacy />} />
+          </Route>
+
+          {/* Admin Routes (No MainLayout, no public Navbar/Footer) */}
+          <Route path="/admin/login" element={<AdminLogin />} />
+          <Route path="/admin" element={<ProtectedRoute><AdminDashboard /></ProtectedRoute>} />
+          <Route path="/admin/post/new" element={<ProtectedRoute><PostEditor /></ProtectedRoute>} />
+          <Route path="/admin/post/edit/:id" element={<ProtectedRoute><PostEditor /></ProtectedRoute>} />
+        </Routes>
+      </Router>
+    </AuthProvider>
   );
-}
+          }
